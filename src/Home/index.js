df3fd9c2436fce364c2ee36d06c5427730da3496
Banco <72240716+Junior-mendez@users.noreply.github.com>
2023-05-09 18:43:15 -0500

    se agrega logos y se modifica home.

--- src/Home/index.js
+++ src/Home/index.js
@@ -4,8 +4,17 @@ import Typography from "@mui/material/Typography"
 import Stack from "@mui/material/Stack"
 import { Avatar } from "@material-ui/core";
 import imageUrl from "../static/images/home/foto.png"
+import logoJsURl from "../static/images/home/logo_js.png"
+import logoReactURl from "../static/images/home/logo_react.png"
+import logoANURl from "../static/images/home/logo_an.png"
+import saludoURL from "../static/images/home/saludo.png"
+import javaURL from "../static/images/home/logo_java.png"
+import csharpURL from "../static/images/home/logo_csharp.png"
+import springURL from "../static/images/home/logo_spring.png"
 import ContactMailIcon from '@mui/icons-material/ContactMail';
 import PlaceIcon from '@mui/icons-material/Place';
+import GithubIcon from '@mui/icons-material/GitHub'
+import { IconButton, ImageList, ImageListItem } from "@mui/material";
 export default function Header() {
     return(
         <Grid container spacing={2} columns={16}>
@@ -25,12 +34,35 @@ export default function Header() {
                 <Typography variant="h6" style={{
                     marginTop:"10px"
                 }}>Backen Developer</Typography>
-                <Typography variant="body1" ><ContactMailIcon fontSize="inherit"></ContactMailIcon> jrmendezlezcano@gmail.com</Typography>
-                 <Typography variant="body1" ><PlaceIcon fontSize="inherit"></PlaceIcon>Trujillo, Perú</Typography>
-                
+                <Grid container columns={3}>
+                <Grid item  xs={8} style={{
+                textAlign:"center"
+            }}>
+                <IconButton href="mailto:jrmendezlezcano@gmail.com"><ContactMailIcon fontSize="inherit"></ContactMailIcon>jrmendezlezcano@gmail.com</IconButton>
+                </Grid>
+                <Grid item  xs={8} style={{
+                textAlign:"center"
+            }}>
+                <IconButton ><PlaceIcon fontSize="inherit"></PlaceIcon>Trujillo, Perú</IconButton>
+                </Grid>
+                <Grid item  xs={8} style={{
+                textAlign:"center"
+            }}>
+                <IconButton target="_blank" variant="body1" href="https://github.com/Junior-mendez" ><GithubIcon fontSize="inherit"></GithubIcon>Junior-mendez</IconButton>
+                </Grid>
+                </Grid>
             </Grid>
             <Grid item  xs={8} style={{textAlign:"left",marginTop:"40px"}}>
-                <Typography variant="h2">Hola, Buen día</Typography>
+                <Typography variant="h2">
+                    <img
+                    width={50}
+                    height={50}
+                    src={saludoURL}
+                    srcSet={saludoURL}
+                    alt={"JS"}
+                    loading="lazy"
+                />
+                    Hola, Buen día</Typography>
                 <Typography variant="body1" style={{marginTop:"40px", marginRight:"80px",justifyContent:"center"}}>
                     Irure ullamco cillum mollit dolor irure tempor dolore consectetur eu aliqua aliquip fugiat 
                     id nisi. Ex ex nulla ut nulla mollit ad aliquip tempor excepteur deserunt esse in ut elit. 
@@ -39,14 +71,65 @@ export default function Header() {
                     id nisi. Ex ex nulla ut nulla mollit ad aliquip tempor excepteur deserunt esse in ut elit. 
                     Sint esse deserunt nostrud quis duis ut dolore enim.</Typography>
                 <Typography variant="h4">Frontend</Typography>
-                <Typography variant="body1" style={{marginTop:"40px", marginRight:"80px",justifyContent:"center"}}>
-                    Irure ullamco cillum mollit dolor irure tempor dolore consectetur eu aliqua aliquip fugiat 
-                    id nisi. Ex ex nulla ut nulla mollit ad aliquip tempor excepteur deserunt esse in ut elit. 
-                    Sint esse deserunt nostrud quis duis ut dolore enim.
-                    Irure ullamco cillum mollit dolor irure tempor dolore consectetur eu aliqua aliquip fugiat 
-                    id nisi. Ex ex nulla ut nulla mollit ad aliquip tempor excepteur deserunt esse in ut elit. 
-                    Sint esse deserunt nostrud quis duis ut dolore enim.</Typography>
+                <ImageList variant="masonry" cols={3} gap={1} sx={{
+                    width: 150,
+                    height: 50,
+                }}>
+                    <ImageListItem>
+                    <img
+                        src={logoJsURl}
+                        srcSet={logoJsURl}
+                        alt={"JS"}
+                        loading="lazy"
+                    />
+                    </ImageListItem>
+                    <ImageListItem>
+                    <img
+                        src={logoReactURl}
+                        srcSet={logoReactURl}
+                        alt={"React"}
+                        loading="lazy"
+                    />
+                    </ImageListItem>
+                    <ImageListItem>
+                    <img
+                        src={logoANURl}
+                        srcSet={logoANURl}
+                        alt={"Angular"}
+                        loading="lazy"
+                    />
+                    </ImageListItem>
+                </ImageList>
                 <Typography variant="h4">Backend</Typography>
+                <ImageList variant="masonry" cols={3} gap={1} sx={{
+                    width: 150,
+                    height: 50,
+                }}>
+                    <ImageListItem>
+                    <img
+                        src={javaURL}
+                        srcSet={javaURL}
+                        alt={"Java"}
+                        loading="lazy"
+                    />
+                    </ImageListItem>
+                    <ImageListItem>
+                    <img
+                        src={csharpURL}
+                        srcSet={csharpURL}
+                        alt={"CSharp"}
+                        loading="lazy"
+                    />
+                    </ImageListItem>
+                    <ImageListItem>
+                    <img
+                        src={springURL}
+                        srcSet={springURL}
+                        alt={"Spring"}
+                        loading="lazy"
+                    />
+                    </ImageListItem>
+                </ImageList>
                 <Typography variant="body1" style={{marginTop:"40px", marginRight:"80px",justifyContent:"center"}}>
                     Irure ullamco cillum mollit dolor irure tempor dolore consectetur eu aliqua aliquip fugiat 
                     id nisi. Ex ex nulla ut nulla mollit ad aliquip tempor excepteur deserunt esse in ut elit. 
@@ -54,6 +137,13 @@ export default function Header() {
                     Irure ullamco cillum mollit dolor irure tempor dolore consectetur eu aliqua aliquip fugiat 
                     id nisi. Ex ex nulla ut nulla mollit ad aliquip tempor excepteur deserunt esse in ut elit. 
                     Sint esse deserunt nostrud quis duis ut dolore enim.</Typography>
+                <Typography variant="body1" style={{marginTop:"40px", marginRight:"80px",justifyContent:"center"}}>
+                Irure ullamco cillum mollit dolor irure tempor dolore consectetur eu aliqua aliquip fugiat 
+                id nisi. Ex ex nulla ut nulla mollit ad aliquip tempor excepteur deserunt esse in ut elit. 
+                Sint esse deserunt nostrud quis duis ut dolore enim.
+                Irure ullamco cillum mollit dolor irure tempor dolore consectetur eu aliqua aliquip fugiat 
+                id nisi. Ex ex nulla ut nulla mollit ad aliquip tempor excepteur deserunt esse in ut elit. 
+                Sint esse deserunt nostrud quis duis ut dolore enim.</Typography>
                     
             </Grid>
         </Grid>
