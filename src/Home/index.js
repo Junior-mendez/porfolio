543e8d4a1fc14a795e76a9fa2958f83c945c8fb1
Angel Mendez <angelpieromendezquinones@gmail.com>
2023-05-12 17:54:51 -0500

    actualización de texto

--- src/Home/index.js
+++ src/Home/index.js
@@ -66,13 +66,16 @@ export default function Home() {
                 />
                     Hola, Buen día</Typography>
                 <Typography variant="body1" style={{marginTop:"40px", marginRight:"80px",justifyContent:"center"}}>
-                    Soy un desarrollador de software, 
-                    Irure ullamco cillum mollit dolor irure tempor dolore consectetur eu aliqua aliquip fugiat 
-                    id nisi. Ex ex nulla ut nulla mollit ad aliquip tempor excepteur deserunt esse in ut elit. 
-                    Sint esse deserunt nostrud quis duis ut dolore enim.
-                    Irure ullamco cillum mollit dolor irure tempor dolore consectetur eu aliqua aliquip fugiat 
-                    id nisi. Ex ex nulla ut nulla mollit ad aliquip tempor excepteur deserunt esse in ut elit. 
-                    Sint esse deserunt nostrud quis duis ut dolore enim.</Typography>
+                    Soy un desarrollador de software, Es un hecho establecido hace demasiado tiempo que un lector 
+                    se distraerá con el contenido del texto de un sitio mientras que mira su diseño. El punto de 
+                    usar Lorem Ipsum es que tiene una distribución más o menos normal de las letras, al contrario
+                     de usar textos como por ejemplo "Contenido aquí, contenido aquí". Estos textos hacen parecerlo 
+                     un español que se puede leer. Muchos paquetes de autoedición y editores de páginas web usan el 
+                     Lorem Ipsum como su texto por defecto, y al hacer una búsqueda de "Lorem Ipsum" va a dar por
+                      resultado muchos sitios web que usan este texto si se encuentran en estado de desarrollo. 
+                      Muchas versiones han evolucionado a través de los años, algunas veces por accidente, otras
+                       veces a propósito (por ejemplo insertándole humor y cosas por el estilo).
+                    </Typography>
                 <Typography variant="h4">Frontend</Typography>
                 <ImageList variant="masonry" cols={3} gap={1} sx={{
                     width: 150,
