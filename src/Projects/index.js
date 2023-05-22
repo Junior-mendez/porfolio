3ed3d914e9383328ed581edc81c69d6b6f7ba19e
Banco <72240716+Junior-mendez@users.noreply.github.com>
2023-05-21 20:27:12 -0500

    Se modifica pag. projects

--- src/Projects/index.js
+++ src/Projects/index.js
@@ -1,11 +1,40 @@
 import * as React from "react"
-import { Typography } from "@mui/material";
+import { Grid, Typography } from "@mui/material";
 
 export default function Projects() {
     return(
-        <Typography variant="h4">
-            Projects
-        </Typography>
+        <Grid container spacing={2} columns={16}>
+            <Grid item  xs={16} md={8} style={{
+                textAlign:"left",
+                marginLeft:"80px",
+                marginTop:"40px"
+            }}>
+                <Typography style={{
+                    fontWeight:700,
+                    textTransform:"none"
+                }} variant="h2"
+                component="div" fontFamily={[
+                    '-apple-system',
+                    'BlinkMacSystemFont',
+                    '"Segoe UI"',
+                    'Roboto',
+                    '"Helvetica Neue"',
+                    'Arial',
+                    'sans-serif',
+                    '"Apple Color Emoji"',
+                    '"Segoe UI Emoji"',
+                    '"Segoe UI Symbol"',
+                  ].join(',')}>
+                    Projects
+                </Typography>
+                <Typography style={{
+                marginTop:"20px"
+            }}>Algunos de los proyectos que he construido o de los que he sido parte.</Typography>
+            </Grid>
+            <Grid item  xs={16} md={8} style={{
+                textAlign:"center"
+            }}></Grid>
+        </Grid>
     );
     
 }
